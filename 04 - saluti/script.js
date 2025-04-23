1cb9196ea3af2e48d3c98638c1ab54693189c7e9
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-23 14:56:06 +0200

    feat : esercizio 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,17 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+/**
+ * Prende una stringa e restituisce saluto + nome
+ * @param {*} string 
+ * @returns saluto + nome
+ */
+
+const saluto = (string) => `Ciao ${string} !!!`
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let salutoNome = saluto(name)
+console.log(salutoNome)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
