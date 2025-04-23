1baa58648eaca4757ab0f0d650b3c0eae3143ad0
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-23 14:33:10 +0200

    feat : esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,32 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+// let letter = prompt("Dammi un'iniziale")
+let letter = "a"
+
 
 // Dichiara la funzione qui.
 
+const filtraPerIniziale = (array, initial) => {
+
+    let arrayFiltrata = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i][0].toUpperCase() === initial.toUpperCase()) {
+
+            arrayFiltrata.push(array[i])
+
+        }
+    }
+
+    return arrayFiltrata
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const arrayFiltrataPerIniziale = filtraPerIniziale(names, letter)
+console.log(arrayFiltrataPerIniziale)
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
