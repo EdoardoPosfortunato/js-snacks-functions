b5c2911de1d116f4784b175635f3aee2232b1670
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-23 14:49:21 +0200

    feat : esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,33 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * Prende uan stringa e restituisce il numero di vocali
+ * @param {*} string 
+ * @returns numero di vocali
+ */
+
+const numbersOfVowels = (string) => {
+
+    let contatore = 0;
+    const vowels = "aeiou"
+
+    for (let i = 0; i < string.length; i++) {
+
+        if (vowels.toUpperCase().includes(string[i].toUpperCase())) {
+
+            contatore++;
+
+        }
+    }
+
+    return contatore;
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const numeroDiVocali = numbersOfVowels(word);
+console.log(numeroDiVocali)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
