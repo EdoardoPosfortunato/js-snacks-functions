32eb0b089740c0bdb50b3404136d83145c2f7982
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-26 14:58:19 +0200

    feat : esercizio bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -6,14 +6,50 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-const data = 
+
+const now = new Date();
+console.log(now.toLocaleTimeString());
+
+
+const hoursNumb = now.getHours()
+const hoursString = hoursNumb.toString().padStart(2, '0');
+// console.log(hoursNumb);
+console.log(hoursString);
+
 
 
 // Dichiara la funzione qui.
 
+/**
+ * 
+ * @param {auto}
+ * @returns {string}
+ */
+
+// ESEMPIO di FUNZIONE (controlla se una parola è PALINDROMA)
+
+const hello = (number) => {
+
+    let saluto = ""
+
+    if (number >= 6 &&  number < 13){
+        saluto = "Buongiorno"
+    } else if (number >= 13 &&  number < 17){
+        saluto = "Buon pomeriggio"
+    } else if (number >= 17 &&  number < 24){
+        saluto = "Buona Sera"
+    } else if (number >= 0 &&  number < 6){
+        saluto = "Vai a Letto"
+    }
+
+    return saluto
+    
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let saluto = hello(hoursNumb)
+console.log(saluto)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
